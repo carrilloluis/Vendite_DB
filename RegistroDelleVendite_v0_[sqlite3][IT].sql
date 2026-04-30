1d35dd39fa5edcc9a39bd2b1d0507462dbfbeadf
Luis Carrillo Gutiérrez <carrilloluis@users.noreply.github.com>
2026-04-29 20:59:57 -0400

    Update RegistroDelleVendite_v0_[sqlite3][IT].sql

--- RegistroDelleVendite_v0_[sqlite3][IT].sql
+++ RegistroDelleVendite_v0_[sqlite3][IT].sql
@@ -7,9 +7,9 @@
 DROP TABLE IF EXISTS [Categorie];
 CREATE TABLE IF NOT EXISTS [Categorie]
 (
-	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, -- Identificativo univoco
-	nome VARCHAR(32) NOT NULL,
-	descrizione TEXT NULL -- Descrizione dettagliata
+	[id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, -- Identificativo univoco
+	[nome] VARCHAR(32) NOT NULL,
+	[descrizione] TEXT NULL -- Descrizione dettagliata
 ); -- WITHOUT RowId;
 
 DROP TABLE IF EXISTS [Prodotti];
@@ -35,22 +35,46 @@ CREATE TABLE IF NOT EXISTS [Fornitori]
 	nome VARCHAR(128) NOT NULL
 ); -- WITHOUT RowId;
 
+
+DROP TABLE IF EXISTS [MagazziniMerci];
+CREATE TABLE IF NOT EXISTS [MagazziniMerci]
+(
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	nomeNegozio VARCHAR(64) NOT NULL, 
+	descrizione TEXT NULL,
+	direzione VARCHAR(64) NOT NULL  -- luogo
+);
+
 -- articolo di magazzino / articolo nell'inventario
 DROP TABLE IF EXISTS [ArticoliNellInventario];
 CREATE TABLE IF NOT EXISTS [ArticoliNellInventario] -- stock item
 (
 	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	-- codice
+	UnitáDiVendita VARCHAR(8) NOT NULL DEFAULT 'PKG', -- pacchetto
 	idProdotto INTEGER NOT NULL REFERENCES [Prodotti](id),
 	idFornitore INTEGER NOT NULL REFERENCES [Fornitori](id),
 	KEY [idProdotto, idFornitore]
 ); -- WITHOUT RowId;
 
-DROP TABLE IF EXISTS [Inventario];
-CREATE TABLE IF NOT EXISTS [Inventario]
+DROP TABLE IF EXISTS [Inventari];
+CREATE TABLE IF NOT EXISTS [Inventari]
 (
 	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	-- Identificazione di compra
+	idAcquisto INTEGER NOT NULL, -- REFERENCES [AcquistoDaFornitori](id)
 	idArticoloNellInventario INTEGER NOT NULL REFERENCES [ArticoliNellInventario](id),
 	quantita DECIMAL(10, 3) DEFAULT 1.00,
     dataDiIngresso DATETIME DEFAULT CURRENT_TIMESTAMP -- Data creazione
+); -- WITHOUT RowId;
+
+DROP TABLE IF EXISTS [TransazioneDiVendita];
+CREATE TABLE IF NOT EXISTS [TransazioneDiVendita]
+(
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	-- Identificazione di vendita
+	idVendita INTEGER NOT NULL, -- REFERENCES [VenditaAiClienti](id)
+	idArticoloNellInventario INTEGER NOT NULL REFERENCES [ArticoliNellInventario](id),
+	quantita DECIMAL(10, 3) DEFAULT 1.00,
+	dataDiUscita DATETIME DEFAULT CURRENT_TIMESTAMP
 ); -- WITHOUT RowId;
